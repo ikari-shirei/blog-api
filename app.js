afa189dc3d14d40cdf8963591a004c5f74c7dd63
ikari-shirei <algrsyu@protonmail.com>
2022-03-28 05:26:13 +0300

    Set up

--- app.js
+++ app.js
@@ -3,9 +3,30 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
+var compression = require('compression')
+var helmet = require('helmet')
+
+const mongoose = require('mongoose')
+require('dotenv').config()
+
 var indexRouter = require('./routes/index')
 
 var app = express()
+
+/* mongoDB */
+
+const mongoDb = process.env.DB_URI
+
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true })
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'mongo connection error'))
+mongoose.connection.readyState === 2 ? console.log('MongoDB connected') : ''
+
+/* Middlewares */
+
+app.use(compression())
+app.use(helmet())
 
 app.use(logger('dev'))
 app.use(express.json())
